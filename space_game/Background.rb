9f0297cfa78701e80aa60ba429527bbf8602a46a
Albert Llebaria Fabriàs <albertllebariafabrias@MacBook-MacBook-Pro-de-Albert.local>
2019-12-13 12:21:24 +0100

    backgorund init

--- space_game/Background.rb
+++ space_game/Background.rb
@@ -1,3 +1,6 @@
 class Background
     
+    def initialize(numero_estrellas)
+        
+    end
 end
--- space_game/Star.rb
+++ space_game/Star.rb
@@ -1,14 +1,21 @@
 require "./Point"
+require "gosu"
 
 class Star
     attr_accessor :position
-    
+
     def initialize(point)
-        @image = set_random_image
+        @image = Gosu::Image.new(set_random_image)
         @position = point
     end
     
     private
     def set_random_image
+        random_number = rand()
+        path = "assets/star1.jpg"
+        if random_number => 0.5
+            path = "assets/star2.png"
+        end
+        return path
     end
 end
