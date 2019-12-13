3435f11b3db0f528bb5ff1a9a835f1587cb526ce
Albert Llebaria Fabriàs <albertllebariafabrias@MacBook-MacBook-Pro-de-Albert.local>
2019-12-13 12:08:39 +0100

    Removed pruebas in master

--- space_game/Star.rb
+++ space_game/Star.rb
@@ -1,10 +1,13 @@
 require "./Point"
 
 class Star
-    def initialize(Point)
+    attr_accessor :position
+    
+    def initialize(point)
         @image = set_random_image
+        @position = point
     end
-
+    
     private
     def set_random_image
     end
