d05ee9d6105cb50e67628bf61895cab944669560
Albert Llebaria Fabriàs <albertllebariafabrias@MacBook-MacBook-Pro-de-Albert.local>
2019-12-13 12:22:34 +0100

    merged master

--- space_game/Background.rb
+++ space_game/Background.rb
@@ -1,6 +1,15 @@
 class Background
     
     def initialize(numero_estrellas)
-        
+        @stars_array = 
+    end
+
+    private 
+    def generar_stars_array(numero_estrellas)
+        array = []
+        x_position = 0
+        numero_estrellas.times do
+
+        end
     end
 end
