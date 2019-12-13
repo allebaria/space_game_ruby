f5bb8090035a0b608215a0e64b99d6a1e14a11b8
Albert Llebaria Fabriàs <albertllebariafabrias@MacBook-MacBook-Pro-de-Albert.local>
2019-12-13 12:28:51 +0100

    Background with stars

--- space_game/Background.rb
+++ space_game/Background.rb
@@ -1,15 +1,26 @@
 class Background
     
     def initialize(numero_estrellas)
-        @stars_array = 
+        @stars_array = generar_stars_array(numero_estrellas)
     end
 
     private 
     def generar_stars_array(numero_estrellas)
         array = []
         x_position = 0
-        numero_estrellas.times do
+        y_position = 480
+        x_step = 640. / numero_estrellas
+        point = Point.new(x_position, y_position)
 
+        numero_estrellas.times do
+            y_position = rand() * 480
+            point.x = x_position
+            point.y = y_position
+            star = Star.new(point)
+            array.push(star)
+            x_position += x_step
         end
+
+        return array
     end
 end
